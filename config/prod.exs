979ca70bb36bb188b5f01954fea35a64681baa1f
Lucian Leustean <lucian.leustean@gmail.com>
2019-09-07 12:36:49 +0300

    Setup production for Gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :pw_demo, PwDemoWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
+  server: true,
+  url: [host: "pw-demo.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
@@ -52,4 +55,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
